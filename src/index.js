82c816de3478f6017d255c109cf94dac56483a88
mackinley <mack.drummond@gmail.com>
2024-03-18 10:15:40 +1100

    housekeeping

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import App from './App.tsx'
+import './index.css' 
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); 
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+root.render( <App /> );
